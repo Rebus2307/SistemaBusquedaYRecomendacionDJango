03e5914f1bd321d1139d3ffa806b9f883063c4e5
Wilfrido Merlin <willy2019031000merlin@gmail.com>
2025-06-25 02:25:42 -0600

    apiviews

--- core/api_views.py
+++ core/api_views.py
@@ -1,11 +1,19 @@
 from rest_framework import viewsets, permissions
 from .models import Usuario, LibroFavorito, PeliculaFavorita
-from .serializers import UsuarioSerializer, LibroFavoritoSerializer, PeliculaFavoritaSerializer
+from .serializers import UsuarioSerializer, UsuarioRegistroSerializer, LibroFavoritoSerializer, PeliculaFavoritaSerializer
 
 class UsuarioViewSet(viewsets.ModelViewSet):
     queryset = Usuario.objects.all()
-    serializer_class = UsuarioSerializer
-    permission_classes = [permissions.IsAuthenticated]
+
+    def get_permissions(self):
+        if self.action == 'create':
+            return [permissions.AllowAny()]
+        return [permissions.IsAuthenticated()]
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return UsuarioRegistroSerializer
+        return UsuarioSerializer
 
 class LibroFavoritoViewSet(viewsets.ModelViewSet):
     queryset = LibroFavorito.objects.all()
