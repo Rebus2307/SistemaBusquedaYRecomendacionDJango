2bd750d95929e9ade2e77d934f1d1f9c1d75d534
Wilfrido Merlin <willy2019031000merlin@gmail.com>
2025-06-25 02:54:44 -0600

    views

--- core/api_views.py
+++ core/api_views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets, permissions
 from .models import Usuario, LibroFavorito, PeliculaFavorita
 from .serializers import UsuarioSerializer, UsuarioRegistroSerializer, LibroFavoritoSerializer, PeliculaFavoritaSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 class UsuarioViewSet(viewsets.ModelViewSet):
     queryset = Usuario.objects.all()
@@ -14,6 +16,11 @@ class UsuarioViewSet(viewsets.ModelViewSet):
         if self.action == 'create':
             return UsuarioRegistroSerializer
         return UsuarioSerializer
+
+    @action(detail=False, methods=['get'], permission_classes=[permissions.IsAuthenticated])
+    def me(self, request):
+        serializer = self.get_serializer(request.user)
+        return Response(serializer.data)
 
 class LibroFavoritoViewSet(viewsets.ModelViewSet):
     queryset = LibroFavorito.objects.all()
